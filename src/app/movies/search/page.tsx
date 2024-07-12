339769389e9486f4adab3aa1050d8fa9cba76ee0
james rogers <jd2rogers2@gmail.com>
2024-07-12 12:36:39 -0700

    suspense around components that useSearchParams

--- src/app/movies/search/page.tsx
+++ src/app/movies/search/page.tsx
@@ -2,13 +2,16 @@
 
 import MoviesSearchForm from '@/app/components/MoviesSearchForm';
 import MovieResultsList from '@/app/components/MovieResultsList';
+import { Suspense } from 'react';
 
 
 export default async function MoviesSearch({ searchParams }: { searchParams: { [key: string]: string } }) {
   return (
     <main style={{ marginTop: '20px' }}>
       <h1>Movie Search! :)</h1>
-      <MoviesSearchForm />
+      <Suspense fallback={<p>loading...</p>}>
+        <MoviesSearchForm />
+      </Suspense>
       <MovieResultsList searchParams={searchParams} />
     </main>
   );
